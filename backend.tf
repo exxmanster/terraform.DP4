dd44ee46224ed072b1845f28bd7a4d31c30c9981
exxmanster <exxmanster@gmail.com>
2021-12-19 22:18:20 +0200

    VPC

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
-    backend "s3" {
-        bucket = "terraform-backend.dp4"
-        key = "terraform/backend"
-        region = "eu-central-1"
-    }
+  backend "s3" {
+    bucket = "terraform-backend.dp4"
+    key    = "terraform/backend"
+    region = "eu-central-1"
+  }
 }
--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
-  
+
   tags = {
     Name = "vprofile"
   }
@@ -25,7 +25,7 @@ resource "aws_internet_gateway" "IGW" {
 
 resource "aws_nat_gateway" "gw-NAT-1a" {
   allocation_id = aws_eip.nat_gateway-1a.id
-  subnet_id = aws_subnet.public1.id
+  subnet_id     = aws_subnet.public1.id
 
   tags = {
     Name = "gw-NAT-1a"
@@ -34,7 +34,7 @@ resource "aws_nat_gateway" "gw-NAT-1a" {
 
 resource "aws_nat_gateway" "gw-NAT-1b" {
   allocation_id = aws_eip.nat_gateway-1b.id
-  subnet_id = aws_subnet.public2.id
+  subnet_id     = aws_subnet.public2.id
 
   tags = {
     Name = "gw-NAT-1b"
@@ -42,8 +42,8 @@ resource "aws_nat_gateway" "gw-NAT-1b" {
 }
 
 resource "aws_subnet" "private1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-west-1a"
   tags = {
     Name = "privat1"
@@ -51,34 +51,34 @@ resource "aws_subnet" "private1" {
 }
 
 resource "aws_subnet" "private2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-west-1b"
   tags = {
     Name = "privat2"
 
-    
+
   }
 }
 
 resource "aws_subnet" "public1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.100.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.100.0/24"
   availability_zone = "us-west-1a"
   tags = {
     Name = "public1"
 
   }
-}  
+}
 
 resource "aws_subnet" "public2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.200.0/24"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.200.0/24"
   availability_zone = "us-west-1b"
   tags = {
     Name = "public2"
 
-    
+
   }
 }
 
@@ -86,8 +86,8 @@ resource "aws_route_table" "pub-RT" {
   vpc_id = aws_vpc.main.id
 
   route {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = aws_internet_gateway.IGW.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.IGW.id
   }
 
   tags = {
@@ -99,8 +99,8 @@ resource "aws_route_table" "priv-RT-1a" {
   vpc_id = aws_vpc.main.id
 
   route {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = aws_nat_gateway.gw-NAT-1a.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.gw-NAT-1a.id
   }
 
   tags = {
@@ -112,8 +112,8 @@ resource "aws_route_table" "priv-RT-1b" {
   vpc_id = aws_vpc.main.id
 
   route {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = aws_nat_gateway.gw-NAT-1b.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.gw-NAT-1b.id
   }
 
   tags = {
@@ -148,18 +148,18 @@ resource "aws_security_group" "allow_http" {
 
 
   ingress {
-    description      = "HTTP from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -173,19 +173,19 @@ resource "aws_security_group" "allow_tls" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
 
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -199,18 +199,18 @@ resource "aws_security_group" "allow_ssh" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description      = "SSH from VPC"
-    from_port        = 22 
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
